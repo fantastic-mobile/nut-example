fa3cf69503a2573135029aacf8dd0bdfdf9da55b
Hooray Hu <304327508@qq.com>
2024-06-18 11:54:33 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,7 @@
+> [!IMPORTANT]
+>
+> 本仓库基于 Fantastic-mobile 源码开发，仅作为**UI组件库替换**演示用
+>
+> 将 Vant 替换为 NutUI 的详情操作步骤请查看[官方文档](https://fantastic-mobile.github.io/guide/replace-to-nut.html)
+>
+> 无特别情况将不会与 Fantastic-mobile 最新源码同步，请勿将本仓库源码用于生产环境
---
+++ src/views/index.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const input = ref('')
+const radio = ref(1)
+const rate = ref(3)
+const toggle = ref(true)
+</script>
+
+<template>
+  <PageLayout :navbar="false" tabbar>
+    <PageMain title="Button">
+      <nut-button type="primary">
+        主要按钮
+      </nut-button>
+      <nut-button type="success">
+        成功按钮
+      </nut-button>
+      <nut-button type="default">
+        默认按钮
+      </nut-button>
+      <nut-button type="danger">
+        危险按钮
+      </nut-button>
+      <nut-button type="warning">
+        警告按钮
+      </nut-button>
+    </PageMain>
+    <nut-form class="mx-4">
+      <nut-form-item label="用户名">
+        <nut-input v-model="input" label="文本" placeholder="请输入用户名" />
+      </nut-form-item>
+    </nut-form>
+    <PageMain title="Radio">
+      <nut-radio-group v-model="radio">
+        <nut-radio :label="1">
+          单选框 1
+        </nut-radio>
+        <nut-radio :label="2">
+          单选框 2
+        </nut-radio>
+      </nut-radio-group>
+    </PageMain>
+    <PageMain title="Rate">
+      <nut-rate v-model="rate" />
+    </PageMain>
+    <PageMain title="Switch">
+      <nut-switch v-model="toggle" />
+    </PageMain>
+  </PageLayout>
+</template>
